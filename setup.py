5356d7384dda0fffce550275d21baab31cd6051a
Tuomo Kareoja <tuomo.kareoja@gmail.com>
2019-11-01 18:56:51 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predicting defaulting from credit history. Ubiqum Data Science Bootcamp project.',
+    author='Tuomo Kareoja',
+    license='MIT',
+)
